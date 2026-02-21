6a0ed7408ceca0a6ff587d0c11c7a04b37a5d712
verrad <radyandryfarrel@gmail.com>
2026-02-21 08:43:59 +0700

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const inputNama = document.getElementById("name");
-const inputEmail = document.getElementById("email");
-const btn = document.querySelector("#btn-save");
+$.ajax({
+  url: "https://www.omdbapi.com/?i=tt3896198&apikey=5eef877c&s=harry-potter",
+  success: (results) => {
+    const movies = results.Search;
+    let cards = "";
+    movies.forEach((m) => {
+      cards += `
+        <div class= "col-md4 my-3">
+          <div class="card">
+            <img src="${m.Poster}" class="card-img-top">
+            <div class= "card-body">
+              <h5 class="card-title">${m.Title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+              <a href ="#" class="btn btn-primary">Show Details</a>
+            </div>
+          </div>
+        </div>
+      `;
+      $(".movie-container").html(cards);
+    });
+  },
 
-let users = [];
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const user = {
-    nama: inputNama.value,
-    email: inputEmail.value,
-  };
-
-  users.push(user);
-  console.log(users);
+  error: (e) => {
+    console.log(e.responseText);
+  },
 });
