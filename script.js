74076bbd3b590d5d7b84587e40c7bf3563d007a2
verrad <radyandryfarrel@gmail.com>
2026-03-11 10:12:36 +0700

    menambhakan beberapa fitur untuk mengambil data dari movie

--- script.js
+++ script.js
@@ -1,26 +1,80 @@
-$.ajax({
-  url: "https://www.omdbapi.com/?i=tt3896198&apikey=5eef877c&s=harry-potter",
-  success: (results) => {
-    const movies = results.Search;
-    let cards = "";
-    movies.forEach((m) => {
-      cards += `
-        <div class= "col-md4 my-3">
+$(".serch-button").on("click", function () {
+  $.ajax({
+    url:
+      "https://www.omdbapi.com/?i=tt3896198&apikey=5eef877c&s=" +
+      $(".input-keyword").val(),
+    success: results => {
+      const movies = results.Search;
+      let cards = "";
+      movies.forEach(m => {
+        cards += showCards(m);
+      });
+      $(".movie-container").html(cards);
+
+      $(".modal-detail-button").on("click", function () {
+        $.ajax({
+          url:
+            "https://www.omdbapi.com/?apikey=5eef877c&i=" +
+            $(this).data("imdb"),
+          success: m => {
+            const movieDetail = showMovieDetail(m);
+            $(".modal-body").html(movieDetail);
+          },
+          error: e => {
+            console.log(e.responseText);
+          },
+        });
+      });
+    },
+
+    error: e => {
+      console.log(e.responseText);
+    },
+  });
+});
+
+function showCards(m) {
+  return `
+        <div class= "col-md-4 my-3">
           <div class="card">
             <img src="${m.Poster}" class="card-img-top">
             <div class= "card-body">
               <h5 class="card-title">${m.Title}</h5>
               <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-              <a href ="#" class="btn btn-primary">Show Details</a>
+              <a href ="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal"
+      data-bs-target="#movieDetailModal" data-imdb="${m.imdbID}">Show Details</a>
             </div>
           </div>
         </div>
       `;
-      $(".movie-container").html(cards);
-    });
-  },
+}
 
-  error: (e) => {
-    console.log(e.responseText);
-  },
-});
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+              <div class="row">
+                <div class="col-md-3">
+                  <img src="${m.Poster}" alt="" class="img-fluid" />
+                </div>
+                <div class="col-md">
+                  <ul class="list-group">
+                    <li class="list-group-item">
+                    <h4>${m.Title}(${m.Year})</h4>
+                    </li>
+                    <li class="list-group-item">
+                      <strong>Director : </strong> ${m.Director}
+                    </li>
+                    <li class="list-group-item">
+                      <strong>Actors : </strong> ${m.Actors}
+                    </li>
+                    <li class="list-group-item">
+                      <strong>Writer : </strong> ${m.Writer}
+                    </li>
+                    <li class="list-group-item">
+                      <strong>Plot : </strong><br />
+                      ${m.Plot}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>`;
+}
